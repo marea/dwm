888d20b621dba44315adc6c0d21a46d4d0165e5d
marea <m.arevalo.aragon@gmail.com>
2021-02-13 18:30:25 -0600

    Changed keybinding for dmenu

--- config.def.h
+++ config.def.h
@@ -87,7 +87,7 @@ ResourcePref resources[] = {
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstackvis,  {.i = +1 } },
