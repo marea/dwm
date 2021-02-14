6b6dc72408768bda6230e0f188b6db490fc30d52
marea <m.arevalo.aragon@gmail.com>
2021-02-13 18:27:34 -0600

    Changed font to GoMono and size to 12

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const char *fonts[]          = { "GoMono Nerd Font:size=10", "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "GoMono Nerd Font:size=12" };
+static const char dmenufont[]       = "GoMono Nerd Font:size=12";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
